6d3d3f6d97cdaa67f7a63e53a066f5684750dbad
Grant Reidy <55988669+grant-reidy@users.noreply.github.com>
2023-12-09 18:24:17 -0500

    Update astro.config.mjs

--- theme/astro.config.mjs
+++ theme/astro.config.mjs
@@ -10,6 +10,7 @@ import partytown from "@astrojs/partytown";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://verbind-llc.github.io',
+  base: '/getverbind/theme',
   // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
   sitemap: true,
   // Generate sitemap (set to "false" to disable)
